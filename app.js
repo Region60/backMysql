752d6b1c1ad74c4c63cabc60ff004791fd31c1d0
Region60 <_max_kot@mail.ru>
2021-11-19 23:40:17 +0300

    home

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'uploads')));
 app.use('/api-docs', swaggerUi.serve)
-app.use(helmet())
+//app.use(helmet())
 app.use(compression())
 
 
