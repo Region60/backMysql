682f4defb1578806bbcead7505d58dba4249f3ff
Region60 <_max_kot@mail.ru>
2021-11-25 18:02:22 +0300

    work 25-11-2021

--- public/dataBase.js
+++ public/dataBase.js
@@ -4,60 +4,56 @@ let NAME_TABLE = 'users'
 const colors = require('colors')
 
 
-const connectionMysql = mysql.createConnection({
+const createPoolMysql = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'qwerty'
+    password: 'qwerty',
+    database: NAME_DB,
+    waitForConnections: true,
+    connectionLimit:10,
+    queueLimit: 0
 })
 
-function connectDataBase() {
-    connectionMysql.connect(function (err) {
-        if (err) {
-            return console.error('Ошибка: '.red + err.message)
-        } else {
-            console.log("Подключение к серверу MySQL установаленно".green)
-        }
-    })
-}
+
 
 function createDataBase() {
-    connectionMysql.query('SHOW DATABASES;',
+    createPoolMysql.query('SHOW DATABASES;',
         function (err, results, fields) {
             if (err) {
-                return console.error('Ошибка при чтении списка баз данных' + err)
+                return console.error('Ошибка при чтении списка баз данных.'.bgRed + err)
             } else {
                 if (results.find((i) => i.Database === NAME_DB)) {
                     console.log(`База данных ${NAME_DB} уже создана`.green)
                 } else {
-                    connectionMysql.query(`CREATE DATABASE ${NAME_DB}`,
+                    createPoolMysql.query(`CREATE DATABASE ${NAME_DB}`,
                         function (err) {
                             if (err) {
-                                return console.error('Ошибка при создании базы данных: ${NAME_DB} '.red + err)
+                                return console.error('Ошибка при создании базы данных: ${NAME_DB} '.bgRed + err)
                             } else {
                                 console.log(`База данных ${NAME_DB} создана`.green)
                             }
                         })
                 }
-                connectionMysql.query(`USE ${NAME_DB}`,
+                createPoolMysql.query(`USE ${NAME_DB}`,
                     function (err) {
                         if (err) {
-                            return console.error('Ошибка при подключении базы данных: '.red + err)
+                            return console.error('Ошибка при подключении базы данных.'.red + err)
                         } else {
                             console.log(`База данных ${NAME_DB} подключена`.green)
                         }
                     })
-                connectionMysql.query(`SHOW TABLES FROM ${NAME_DB} LIKE '${NAME_TABLE}';`,
+                createPoolMysql.query(`SHOW TABLES FROM ${NAME_DB} LIKE '${NAME_TABLE}';`,
                     function (err, results) {
                         if (err) {
-                            return console.error(`Ошибка при провеке таблицы ${NAME_TABLE}: `.red + err)
+                            return console.error(`Ошибка при провеке таблицы ${NAME_TABLE}.`.bgRed + err)
                         } else {
                             if (results.find((i) => i[`Tables_in_appdb (${NAME_TABLE})` === NAME_TABLE])) {
                                 console.log(`таблица ${NAME_TABLE} уже существует`.green)
                             } else {
-                                connectionMysql.query(`CREATE TABLE ${NAME_TABLE} (Id int primary key auto_increment,UserEmail varchar(20),UserPassword varchar(20),FirstName varchar(20));`,
+                                createPoolMysql.query(`CREATE TABLE ${NAME_TABLE} (Id int primary key auto_increment,UserEmail varchar(20),UserPassword varchar(20),FirstName varchar(20));`,
                                     function (err) {
                                         if (err) {
-                                            return console.error(`Ошибка создания таблицы: ${NAME_TABLE}`.red + err)
+                                            return console.error(`Ошибка создания таблицы: ${NAME_TABLE}.`.bgRed + err)
                                         } else {
                                             console.log(`таблица '${NAME_TABLE}' создана`.green)
                                         }
@@ -70,36 +66,28 @@ function createDataBase() {
         })
 }
 
-function findUser(userEmail) {
-    let user
-    connectionMysql.query(`SELECT * FROM ${NAME_TABLE} WHERE UserEmail = '${userEmail}'`,
-        function (err, result) {
-            if (err) {
-                return console.error(`Ошибка при поиске пользователя ${userEmail}: `.red + err)
-            } else {
-                user = result[0]
+ function findUser(userEmail) {
+    try {
+        return  createPoolMysql.promise().query(`SELECT * FROM ${NAME_TABLE} WHERE UserEmail = '${userEmail}'`)
 
-            }
-        })
-    console.log(user)
-
-    return user
+    }catch(e){
+        console.log(e.message.bgRed)
+    }
 }
 
 
 function createUser(UserEmail, UserPassword, FirstName) {
-    connectionMysql.query(`INSERT INTO users (UserEmail, UserPassword, FirstName)values('${UserEmail}','${UserPassword}','${FirstName}')`,
+    createPoolMysql.query(`INSERT INTO users (UserEmail, UserPassword, FirstName)values('${UserEmail}','${UserPassword}','${FirstName}')`,
         function (err) {
             if (err) {
-                return console.error('Ошибка при регистрации пользователя: '.red + err)
+                return console.error(`Ошибка при регистрации пользователя: ${UserEmail}.`.bgRed + err)
             } else {
-                console.log(`пользователь создан`.green)
+                console.log(`пользователь создан `.green)
             }
         })
 }
 
 
-module.exports.connectDataBase = connectDataBase
 module.exports.createDataBase = createDataBase
 module.exports.createUser = createUser
 module.exports.findUser = findUser
